f4792241904d485d6f07e407b52d12e72a70941a
Home <Home@Home-PC>
2020-05-14 23:05:34 +0900

    삼항 조건연산자

---
+++ java8_kb_book/src/test/CondiOper.java
@@ -0,0 +1,14 @@
+package test;
+
+public class CondiOper {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		int x, y;
+		
+		x = (a<b) ? 5 : (a+2);
+		y = (a>b) ? 5 : (a+2);
+		System.out.println(x);
+		System.out.println(y);
+	}
+}
